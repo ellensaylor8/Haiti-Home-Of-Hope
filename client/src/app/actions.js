d7f2348b9fddc286dfb6b447b6b1e57a3ae5eeb2
Ellen Saylor <ellensaylor8@gmail.com>
2024-02-13 21:51:43 -0600

    put stripe key in env file

--- client/src/app/actions.js
+++ client/src/app/actions.js
@@ -1,7 +1,7 @@
+"use server";
 // Set your secret key. Remember to switch to your live secret key in production.
 // See your keys here: https://dashboard.stripe.com/apikeys
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const stripe = require("stripe")("");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const productIds = {
   recurring: {
